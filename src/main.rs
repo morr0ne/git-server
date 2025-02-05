bb3deb90bd588e24abd6dbb00fc5ab288a30e3fe
Federico Maria Morrone <contact@morrone.dev>
2025-02-05 19:51:48 +0100

    Create basic axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::Ipv4Addr;
+
+use anyhow::Result;
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use tracing::debug;
+
+const PORT: u16 = 3344;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, PORT)).await?;
+
+    debug!("Started server on port {PORT}");
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
